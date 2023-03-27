c457c84f9b30a6b687d4e7c751aaa1ce1c28d5ba
jooseppi12 <urijozsef@europe.com>
2023-03-27 09:53:41 +0200

    Add helper cmd to update packages and run build

---
+++ wsbuild.cmd
@@ -0,0 +1,4 @@
+dotnet add .\websharper\WebSharper.Elmish.fsproj package WebSharper --prerelease
+dotnet add .\websharper\WebSharper.Elmish.fsproj package WebSharper.FSharp --prerelease
+
+dotnet fsi wsbuild.fsx
