7aea013757f285a47fd15f83d3f3f9c9375f374a
Jooseppi12 <urijozsef@europe.com>
2023-03-31 13:12:40 +0200

    Update wsbuild.cmd
    Move package to correct folder

--- wsbuild.cmd
+++ wsbuild.cmd
@@ -2,3 +2,5 @@ dotnet add .\websharper\WebSharper.Elmish.fsproj package WebSharper --prerelease
 dotnet add .\websharper\WebSharper.Elmish.fsproj package WebSharper.FSharp --prerelease
 
 dotnet fsi wsbuild.fsx
+
+cp .\websharper\bin\Release\*.nupkg ..\localnuget
