9a07a83ecbc94d798bfc80253e80b056d4b36fd5
Jooseppi12 <urijozsef@europe.com>
2023-03-31 14:04:55 +0200

    Update wsbuild.cmd
    Use localnuget folder

--- wsbuild.cmd
+++ wsbuild.cmd
@@ -1,6 +1,11 @@
+dotnet nuget add source ../localnuget --name LOCALFORELMISH
+
 dotnet add .\websharper\WebSharper.Elmish.fsproj package WebSharper --prerelease
 dotnet add .\websharper\WebSharper.Elmish.fsproj package WebSharper.FSharp --prerelease
 
 dotnet fsi wsbuild.fsx
 
-cp .\websharper\bin\Release\*.nupkg ..\localnuget
+cp ./websharper/bin/Release/*.nupkg ../localnuget
+
+
+dotnet nuget remove source LOCALFORELMISH
