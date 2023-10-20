f1beb1cd06a4e7236f6ecc8045a6c0c6a4ba2b33
Jooseppi12 <urijozsef@europe.com>
2023-10-20 12:51:39 +0200

    Fix update script

--- wsbuild.cmd
+++ wsbuild.cmd
@@ -1,5 +1,8 @@
 dotnet nuget add source %~dp0/../localnuget --name LOCALFORELMISH
 
+dotnet remove .\websharper\WebSharper.Elmish.fsproj package WebSharper
+dotnet remove .\websharper\WebSharper.Elmish.fsproj package WebSharper.FSharp
+
 dotnet add .\websharper\WebSharper.Elmish.fsproj package WebSharper --prerelease
 dotnet add .\websharper\WebSharper.Elmish.fsproj package WebSharper.FSharp --prerelease
 
